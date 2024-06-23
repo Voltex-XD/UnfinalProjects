6731b85799d8de622538e7a20fa55df1cb8ce296
Voltex-XD <96913694+Voltex-XD@users.noreply.github.com>
2024-06-23 18:27:02 +0300

    Add files via upload

---
+++ server.bat
@@ -0,0 +1,9 @@
+@echo off
+:start
+echo Așteptând date de la jocul Roblox...
+set /p requestData=
+echo Date primite de la jocul Roblox: %requestData%
+
+REM Aici poți procesa datele primite și le poți salva într-un fișier sau le poți utiliza pentru alte operațiuni
+
+goto start
